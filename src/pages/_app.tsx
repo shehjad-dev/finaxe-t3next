68c7f398ae572bb59b269e818c3bf533c952a737
shehjad-dev <satauskz@gmail.com>
2023-02-14 02:28:19 +0600

    fix: env fixed

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -3,6 +3,7 @@ import { createBrowserSupabaseClient } from "@supabase/auth-helpers-nextjs";
 import { SessionContextProvider, Session } from "@supabase/auth-helpers-react";
 import { AppProps } from "next/app";
 import "../styles/globals.css";
+import { env } from "../env.mjs";
 
 function MyApp({
   Component,
@@ -12,9 +13,8 @@ function MyApp({
 }>) {
   const [supabase] = useState(() =>
     createBrowserSupabaseClient({
-      supabaseUrl: "https://tmsqazfwbmssgsnkbitt.supabase.co",
-      supabaseKey:
-        "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InRtc3FhemZ3Ym1zc2dzbmtiaXR0Iiwicm9sZSI6ImFub24iLCJpYXQiOjE2NzYyODkyMTgsImV4cCI6MTk5MTg2NTIxOH0.YEbT5WU0qUkvzzyX0kheGKJgnl1wjmYLYnvhmRbvsDY",
+      supabaseUrl: env.NEXT_PUBLIC_SUPABASE_URL,
+      supabaseKey: env.NEXT_PUBLIC_SUPABASE_ANON_KEY,
     })
   );
 
